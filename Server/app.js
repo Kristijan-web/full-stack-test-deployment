272163ee2ce619812386688ba097ece92f949108
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-07-21 00:55:52 +0200

    Finished signup practice

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const toursRouter = require("./routes/toursRouter");
 const usersRouter = require("./routes/userRouter");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json({ limit: "10kb" }));
+app.use(cors());
 
 app.use("/api/v1/tours", toursRouter);
-app.use("api/v1/users", usersRouter);
+app.use("/api/v1/users", usersRouter);
 
 module.exports = app;
